0bce97209bcc15db267216799f4f7478ebd9688a
Fi-Boo <s2008156@student.rmit.edu.au>
2024-11-19 10:30:34 +1100

    temp store

--- js/script.js
+++ js/script.js
@@ -8,6 +8,10 @@ window.addEventListener('resize', function () {
     resizeTimer = setTimeout(function () {
         document.body.classList.remove('no-transition');
     }, 250); // delay in ms to wait for the resize to finish
+
+    if (window.innerWidth > 600) {
+        document.getElementById('menu-toggle').checked = false;
+    }
 });
 
 let prevScrollPos = window.scrollY;
@@ -21,9 +25,24 @@ window.addEventListener('scroll', function () {
 
     if (prevScrollPos < currentScrollPos) {
         header.classList.add('hide');
+        if (window.scrollY < window.innerHeight) {
+            window.scrollTo({
+                top: window.innerHeight, // Scroll to the height of the viewport
+                behavior: 'smooth' // Smooth scrolling effect
+            });
+        }
     } else {
         header.classList.remove('hide');
+        if (currentScrollPos < prevScrollPos && (window.scrollY < (window.innerHeight + 20))) {
+            window.scrollTo({
+                top: 0, 
+                behavior: 'smooth' // Smooth scrolling effect
+            });
+        }
     }
 
     prevScrollPos = currentScrollPos;
+
+    
 });
+
